19e4d97fa70a9f4edd7e77122a0037f448dae104
Hajar2000Elkadiri <123644604+Hajar2000Elkadiri@users.noreply.github.com>
2024-05-01 21:39:15 +0100

    test.py

--- test.py
+++ test.py
@@ -82,7 +82,7 @@ def translate_html(html_code, choice):
 
 lang_array = {lang[i]: langcode[i] for i in range(len(langcode))}
 try :   
-  st.sidebar.image(r"C:\Users\Dell\Desktop\test\Logo Masen VF.png",caption="", use_column_width=True)  
+  st.sidebar.image("https://github.com/Hajar2000Elkadiri/DDTMASEN/blob/main/Logo%20Masen%20VF.png",caption="", use_column_width=True)  
   print("Excel file successfully read.")
 except Exception as e:
   print("Error reading Excel file:", e)
